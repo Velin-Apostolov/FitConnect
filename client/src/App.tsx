574c24f9635a872747f99caf6f8eec5cf4cb9bb3
Velin Apostolov <velin.apostolove@gmail.com>
2025-01-02 19:19:34 +0200

    removed initial vite stylization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
     <h1>FitConnect</h1>
     <h2>Where Trainers Connect with their Clients</h2>
+    <Button className="rounded-md">Button</Button>
     </>
   )
 }
